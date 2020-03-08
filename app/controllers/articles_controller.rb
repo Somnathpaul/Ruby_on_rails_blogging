e8737f3ea0f7454ea192ec9706ab8bef5e932ecd
Somnath Paul <Somnath4x@gmail.com>
2020-03-08 18:53:50 +0530

    modified

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,30 +1,35 @@
 class ArticlesController < ApplicationController
-    # to generate new articles
+    # GET: [to generate new articles]
     def new
        @article = Article.new 
     end
 
-    # to create new articles
+    def index
+      @article = Article.all
+    end
+
+    # POST:  [to create new articles]
     def create
         # to display the values on web after submiting
           # render plain: params[:article].inspect
          
           # to save the values on databse
-            @article = Article.new(article_params)
+          @article = Article.new(article_params)
             if @article.save
             # redirect to specific page
               flash[:notice] = "Article was created"
-              redirect_to articles_path(@article)
+              redirect_to article_path(@article)
              else
               render 'new'
             end
           end
 
-          #show article after creating
+          # GET: [show article after creating]
           def show
             @article = Article.find(params[:id])
           end
 
+          
     private
       def article_params
         params.require(:article).permit(:title, :description)
