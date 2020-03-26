df52d237046cf12fe89c3edf443b57bfd618a5e7
Somnath Paul <Somnath4x@gmail.com>
2020-03-26 13:37:52 +0530

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+    # uniqueness: { case_sensitive: false } doesnot allow duplicate data
     
+    validates :username, 
+    presence: true, 
+    length: {minimum: 3, maximum: 25}, 
+    uniqueness: { case_sensitive: false }
+
+    validates :email,
+    presence: true,
+    length: {minimum: 3, maximum: 64}, 
+    uniqueness: { case_sensitive: false }
 end
